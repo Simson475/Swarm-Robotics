ffcbc3eff542438061c13279101f5c5b726ce67c
Martin Kristjansen <martinkri35@gmail.com>
2021-02-01 08:31:14 +0100

    Updated run-scripts for cluster

--- Simulation/experiment/runtime_scripts/cluster_experiments.sh
+++ Simulation/experiment/runtime_scripts/cluster_experiments.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-#SBATCH --time=1:00:00
-#SBATCH --mail-user=mk@cs.aau.dk
-#SBATCH --mail-type=FAIL
-#SBATCH --partition=dhabi
-#SBATCH --mem=15000
 
-mkdir -p results
+mkdir -p results_${1}
 
 for i in {1..30}
 do
-    sbatch ./setup_and_run_cluster.sh $i
+    sbatch ./setup_and_run_cluster.sh $i ${1}
 done
--- Simulation/experiment/runtime_scripts/setup_and_run_cluster.sh
+++ Simulation/experiment/runtime_scripts/setup_and_run_cluster.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-#SBATCH --time=1:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mail-user=mk@cs.aau.dk
 #SBATCH --mail-type=FAIL
 #SBATCH --partition=dhabi
 #SBATCH --mem=1500
 
 # Creates variable for the new folder
-new_folder=exp_${1}
+new_folder=exp_${2}_${1}
 mkdir $new_folder
 
 # Copies all the files needed. Needs to be updated to use sym-links
-ln -s $(realpath experiment/trajectory.argos) test/trajectory.argos
-
+ln -s $(realpath experiment/trajectory.argos) ${new_folder}/trajectory.argos
 ln -s $(realpath experiment/argos3-lib) ${new_folder}/argos3-lib
 ln -s $(realpath experiment/bin-Linux) ${new_folder}/bin-Linux
 ln -s $(realpath experiment/argos3) ${new_folder}/argos3
 ln -s $(realpath experiment/libCTrajectoryLoopFunctions.so) ${new_folder}/libCTrajectoryLoopFunctions.so
-ln -s $(realpath experiment/libSingleThreadUppaalBot.so) ${new_folder}/libSingleThreadUppaalBot.so
+ln -s $(realpath experiment/libBotImplementations.so) ${new_folder}/libBotImplementations.so
 ln -s $(realpath experiment/planning_blueprint.xml) ${new_folder}/planning_blueprint.xml
 ln -s $(realpath experiment/points.json) ${new_folder}/points.json
 ln -s $(realpath experiment/run_a_scene.sh) ${new_folder}/run_a_scene.sh
@@ -28,10 +27,10 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./argos3-lib && ./argos3 -c ./trajectory
     cd ..
 
     # Moves the results and giving it a proper name
-    mv ${new_folder}/data.csv results/${new_folder}.csv
+    mv ${new_folder}/data.csv results_${2}/${new_folder}.csv
 
     # Remove all the data.
     rm -r $new_folder
 } || {
-    echo "The execution of ARGOS failed. See log-files for more details."; exit -10;
+    echo "The execution of ARGOS failed. See log-files for more details."; exit -1;
 }
