29ca293a20801bf7df5a1e74a4e7d75281fa9ce3
Suprezer <jmo@live.dk>
2022-04-29 14:52:10 +0200

    Added multithreading to SuperScalable experiment

--- Simulation/Pathfinding/Experiments/Experiments/SuperScalable.cpp
+++ Simulation/Pathfinding/Experiments/Experiments/SuperScalable.cpp
@@ -1,4 +1,6 @@
 #include "HighLevelCBS.hpp"
+#include <thread>
+#include <future>
 
 int getAvailableStation(std::vector<int> &stations)
 {
@@ -6,6 +8,13 @@ int getAvailableStation(std::vector<int> &stations)
     int station = stations[stationIndex];
     stations.erase(stations.begin() + stationIndex);
     return station;
+}
+
+uint64_t tempFunc(std::shared_ptr <Graph> graph, std::vector<AgentInfo> agents){
+    std::chrono::steady_clock::time_point experimentBeginTime = std::chrono::steady_clock::now();
+    Solution solution = HighLevelCBS::get_instance().findSolution(graph, agents, LowLevelCBS::get_instance());
+    return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - experimentBeginTime).count();
+
 }
 
 int main(int argc, char *argv[])
@@ -18,6 +27,7 @@ int main(int argc, char *argv[])
 
     for (int size = 1; size <= 20; size++)
     {
+        int threads = 100;
         int height = 2 * size;
         int width = 2 * size;
         int verticesMade = 0;
@@ -49,43 +59,47 @@ int main(int argc, char *argv[])
             if(agentCount >= width*height - 2) continue;
             int64_t timeSpent=0;
             int maxLoops =100;
-            for (int loops = 0; loops < maxLoops; loops++)
+            for (int loops = 0; loops < maxLoops; loops+= threads)
             {
-                std::cout << "Running experiment with " << agentCount << " agents..\n";
+                std::cout << "Running experiment with " << agentCount << " agents.." << loops << "\n";
                 std::cout.flush();
                 std::string experimentResultFile = experimentResultDir + "/SuperScalable.txt";
                 // Remove any existing results if they exist
                 // remove(&experimentResultFile[0]);
                 logger.setLogFile(experimentResultFile);
-
                 // Arrange experiment
-
-                std::vector<AgentInfo> agents{(long unsigned int)agentCount};
-                std::vector<int> freeStarts(width * height);
-                std::vector<int> freeGoals(width * height);
-                for (int i = 0; i < width * height; ++i)
+                std::vector<std::vector<AgentInfo>> agents(threads, std::vector<AgentInfo>({(long unsigned int)agentCount}));
+                for (int j = 0; j < threads; j++)
                 {
-                    freeStarts[i] = i;
-                    freeGoals[i] = i;
-                }
+                    std::vector<int> freeStarts(width * height);
+                    std::vector<int> freeGoals(width * height);
+                    for (int i = 0; i < width * height; ++i)
+                    {
+                        freeStarts[i] = i;
+                        freeGoals[i] = i;
+                    }
 
-                for (int i = 0; i < agentCount; ++i)
-                {
-                    auto startVertex = vertices[getAvailableStation(freeStarts)];
-                    auto goalVertex = vertices[getAvailableStation(freeGoals)]; // TODO set to actual goal
-                    std::cout <<"agent"<< i <<" Start:"<< startVertex->getId()<< " Goal:"<< goalVertex->getId() << "\n";
-                    agents[i] = AgentInfo(i, Action(0, startVertex, startVertex, 0), goalVertex);
+                    for (int i = 0; i < agentCount; ++i)
+                    {
+                        auto startVertex = vertices[getAvailableStation(freeStarts)];
+                        auto goalVertex = vertices[getAvailableStation(freeGoals)]; // TODO set to actual goal
+                        //std::cout <<"agent"<< i <<" Start:"<< startVertex->getId()<< " Goal:"<< goalVertex->getId() << "\n";
+                        agents[j][i] = AgentInfo(i, Action(0, startVertex, startVertex, 0), goalVertex);
+                    }
                 }
 
                 // Act
-                std::chrono::steady_clock::time_point experimentBeginTime = std::chrono::steady_clock::now();
-                Solution solution = HighLevelCBS::get_instance().findSolution(graph, agents, LowLevelCBS::get_instance());
-                auto experimentTime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - experimentBeginTime).count();
-                // Results
-                timeSpent+=experimentTime;
-
+                std::vector<std::future<uint64_t>> futures(threads);
+                for (int i = 0; i < threads; i++)
+                {
+                    futures[i] = std::async(std::launch::async,tempFunc, graph, agents[i]);
+                }
+                for (int i = 0; i < threads; i++)
+                {
+                    timeSpent += futures[i].get();
+                }
             }
-            // When plotting in pgfplots, the first element becomes the x-axis, 2. becomes the y-axis and 3. one becomes the z-axis
+            // When plotting in pgfplots, the first element becomes the x-axis, 2. becomes the y-axis and the 3. one becomes the z-axis
             if (agentCount == 1) continue;
             (*logger.begin()) << "" << agentCount << " " << width << " " << timeSpent/maxLoops << "\n";
             logger.end();
